7861f9a8e734ed8ef153dd5540046e0816391b63
devmahin <mahinhowlader123@gmail.com>
2024-01-26 18:13:37 +0600

    task 3

--- script.js
+++ script.js
@@ -104,3 +104,72 @@ let isNaN2 = isNaN("333")
 console.log(isNaN2)
 let isNaN3 = isNaN(343)
 console.log(isNaN3)
+
+
+
+
+
+// task 1
+var oneKgApple = 350;
+var oneKgOrange = 350;
+var shopeKeperPaid = 1000;
+var result = (shopeKeperPaid - (oneKgApple+ oneKgOrange));
+
+console.log(result);
+
+
+// // avrage গড় নম্বর
+// Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+// Sample Input:
+// 75.25, 65, 80, 35.45, 99.50
+
+// Sample Output:
+// 71.04
+
+const Mathematics = 75.25;
+const Biology = 65;
+const Chemistry = 80;
+const Physics = 35.45;
+const Bangla = 99.50;
+
+const totalSubject = 5;
+const resultAvg = (Mathematics+ Biology+ Chemistry+ Physics+ Bangla) / totalSubject
+console.log(parseFloat(resultAvg.toFixed(2)))
+
+
+
+
+
+
+// task 3
+
+// const total  = 119;
+// const give = 5;
+// const resultRemainder = total % give;
+// console.log(resultRemainder)
+
+
+
+
+
+
+
+
+// Task-4
+// What will be the result of the following codes:
+
+// var a = isNaN(‘11’);
+
+// var a = isNaN(2-10);
+
+// Explain your answers
+var a = isNaN(11);
+console.log(a)
