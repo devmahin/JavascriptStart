bed20ce14b4056c9718927b6db78990c91ca04f7
devmahin <mahinhowlader123@gmail.com>
2024-01-26 08:03:43 +0600

    addition substraction multiplication divition reminder

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ console.log(infi)
 
 const subtraction = -50 / 0;
 console.log(subtraction) 
+
+
+// addition subtraction divition multiplication remainder
+var add1 = 45;
+var add2 = 23;
+console.log(add1 + add2)
+
+
+
+var sub1 = 100;
+var sub2 = 50;
+console.log(sub1 - sub2)
+
+
+// divition
+var div1 = 50;
+var div2 = 5;
+console.log(div1 / div2)
+
+
+// multiplication
+var mul = 5;
+var mul2 = 2;
+console.log(mul * mul2)
+
+
+// remainder
+var remin = 50;
+var remainder2 = 9;
+console.log(remin % remainder2)
